6ef97e20aa010a8187e5dea7a52e4ef2be1a4499
kusum87 <kv4975206@gmail.com>
2024-12-23 19:12:44 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Cust_ERC20 is ERC20 {
+    
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    }
+
+    function mint(uint256 amount) public {
+        _mint(msg.sender, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function customTransfer(address to, uint256 amount) public returns (bool) {
+        
+        require(to != address(0), "Transfer to the zero address is not allowed");
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+
+        _transfer(msg.sender, to, amount);
+
+        return true;
+    }
+}
